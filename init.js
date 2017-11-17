a08026e93f9b78476d9770f79f2bb90f54226de4
Nathan Huesmann <nathan@chefd.com>
2017-11-17 12:32:37 -0800

    Add test folders to init function

--- init.js
+++ init.js
@@ -1,22 +1,24 @@
 const fs = require('fs-extra');
 const jsonfile = require('jsonfile');
 
-const directories = [
-  `${__dirname}/config`,
-  `${__dirname}/csv/split-by-tnt/archive`,
-  `${__dirname}/csv/split-by-tnt/lasership-zipcodes`,
-  `${__dirname}/csv/split-by-tnt/split-csvs`,
-  `${__dirname}/csv/split-by-tnt/split-csvs/non-lasership`,
-  `${__dirname}/csv/submit-lasership-orders/archive`,
-  `${__dirname}/csv/submit-lasership-orders/failed`,
-  `${__dirname}/csv/submit-lasership-orders/tracking_numbers`,
-  `${__dirname}/csv/validate-addresses/archive`,
-  `${__dirname}/csv/validate-addresses/failed`,
-  `${__dirname}/csv/validate-addresses/validated`,
-  `${__dirname}/pdf/archive`,
-  `${__dirname}/pdf/merged-labels`,
-  `${__dirname}/pdf/temp`
-];
+function ensureDirs(directory) {
+  let dirs = [
+    `${directory}/csv/split-by-tnt/archive`,
+    `${directory}/csv/split-by-tnt/lasership-zipcodes`,
+    `${directory}/csv/split-by-tnt/split-csvs`,
+    `${directory}/csv/split-by-tnt/split-csvs/non-lasership`,
+    `${directory}/csv/submit-lasership-orders/archive`,
+    `${directory}/csv/submit-lasership-orders/failed`,
+    `${directory}/csv/submit-lasership-orders/tracking_numbers`,
+    `${directory}/csv/validate-addresses/archive`,
+    `${directory}/csv/validate-addresses/failed`,
+    `${directory}/csv/validate-addresses/validated`,
+    `${directory}/pdf/archive`,
+    `${directory}/pdf/merged-labels`,
+    `${directory}/pdf/temp`,
+  ];
+  dirs.forEach(dir => fs.ensureDirSync(dir));
+}
 
 const configJson = {
   'production': {
@@ -36,5 +38,7 @@ const configJson = {
   }
 };
 
-directories.forEach(directory => fs.ensureDirSync(directory));
+fs.ensureDirSync(`${__dirname}/config`);
+ensureDirs(__dirname);
+ensureDirs(`${__dirname}/test`);
 jsonfile.writeFileSync(`${__dirname}/config/config.json`, configJson, {spaces: 2});
